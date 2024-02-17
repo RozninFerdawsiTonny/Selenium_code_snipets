748d89b93002d232525be8d6bd789c060bc1b1d6
roznin <rozninferdawsi@gmail.com>
2024-02-17 12:53:45 +0600

    TestNG

---
+++ src/test/java/testng/TestCases.java
@@ -0,0 +1,33 @@
+package testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+@Test
+public class TestCases {
+    public void LonginShouldSucceed(){
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("react-burger-menu-btn")).click();
+        String login = driver.findElement(By.id("logout_sidebar_link")).getText().trim();
+        Assert.assertEquals(login, "Logout");
+        String ProductTitle = driver.findElement(By.className("inventory_item_name")).getText().trim();
+        Assert.assertEquals(ProductTitle, "Sauce Labs Backpack");
+        driver.quit();
+
+    }
+}
